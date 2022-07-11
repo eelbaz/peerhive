86be61874be2f9af2028266823f2721965f1372e
eelbaz <mail.eelbaz@gmail.com>
2022-07-11 00:14:31 -0400

    bootstrap update

--- host.go
+++ host.go
@@ -9,7 +9,6 @@ import (
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/host"
-	"github.com/multiformats/go-multiaddr"
 )
 
 func NewHost(ctx context.Context, seed int64, port int) (host.Host, error) {
@@ -30,10 +29,6 @@ func NewHost(ctx context.Context, seed int64, port int) (host.Host, error) {
 	}
 
 	//add bootstrap
-	multiAddr, err := multiaddr.NewMultiaddr("/ip4/172.105.135.138/udp/46351/quic/p2p/12D3KooWD7PXURAfkktsGKAVtwNnvgJKUzVksXCqJcsX7pKHxQKX")
-	if err != nil {
-		panic(err)
-	}
 
 	opts := []libp2p.Option{
 		libp2p.EnableHolePunching(),
@@ -41,7 +36,8 @@ func NewHost(ctx context.Context, seed int64, port int) (host.Host, error) {
 		libp2p.EnableAutoRelay(),
 		libp2p.EnableNATService(),
 		libp2p.EnableRelayService(),
-		libp2p.ListenAddrs(multiAddr),
+		libp2p.ListenAddrStrings("/ip4/172.105.135.138/tcp/46351/p2p/12D3KooWD7PXURAfkktsGKAVtwNnvgJKUzVksXCqJcsX7pKHxQKX"),
+		libp2p.ListenAddrStrings("/ip4/172.105.135.138/udp/46351/quic/p2p/12D3KooWD7PXURAfkktsGKAVtwNnvgJKUzVksXCqJcsX7pKHxQKX"),
 	}
 
 	return libp2p.New(opts...)
